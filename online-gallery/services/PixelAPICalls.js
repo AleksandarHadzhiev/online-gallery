32c8a76f6585adc6d472e4a053c86d62974e1cbd
Hadzhiev,Aleksandar A.A <aleksandar.hadzhiev@student.fontys.nl>
2022-06-07 15:55:14 +0200

    init project init and calls for photos created

---
+++ online-gallery/services/PixelAPICalls.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API_KEY = "563492ad6f9170000100000132ccdf1b436f4e53ab6890c0546a190a";
+const BASE_URL = "https://api.pexels.com/v1/";
+
+const header = {
+    Authorization: API_KEY
+}
+
+class PixelAPICalls{
+    getCuratedPhotos(){
+        return axios.get(BASE_URL+"curated?page=2&per_page=40",{headers:header});
+    }
+
+    getSearchedPhoto(query){
+        return axios.get(BASE_URL+"search?query="+query+"&per_page=40/",{headers:header});
+    }
+}
+
+export default new PixelAPICalls();
