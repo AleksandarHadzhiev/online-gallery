17dae710ad0e15a2d8af63b7e8af5407c2839db7
Hadzhiev,Aleksandar A.A <aleksandar.hadzhiev@student.fontys.nl>
2022-06-10 22:02:46 +0200

    fix-search-component removed unnecessary code

--- online-gallery/component/SearchComponent.js
+++ online-gallery/component/SearchComponent.js
@@ -1,28 +1,7 @@
 import classes from "../styles/SearchComponent.module.css";
 import { FaSearch } from 'react-icons/fa';
-import { useRouter } from "next/dist/client/router";
-
 
 function SearchComponent(props){
-
-    const router = useRouter();
-
-    function searchForPhotos(query){
-        props.searchFor(query);
-    }
-
-    function searchForVideos(query){
-        props.searchFor(query);
-    }
-
-    function locationChecker(query){
-        if(router.pathname==="/videos"){
-            searchForVideos(query);
-        }
-        else {
-            searchForPhotos(query)
-        }
-    }
 
     return (
         <div className={classes.top}>
@@ -30,7 +9,7 @@ function SearchComponent(props){
                 <input onChange={(e)=>{
                     e.preventDefault();
                     if(e.target.value.length>=1){
-                        locationChecker(e.target.value);
+                        props.searchFor(e.target.value);
                     }
                 }} placeholder="Search..." type="text"/>
                 <button><FaSearch/></button>
